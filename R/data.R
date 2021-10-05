eede7efe839605537e872634eff87fea28cf8cd6
Matt Pruett <matthew.pruett@wsu.edu>
2021-10-05 11:18:32 -0700

    move cdl grid data to agclimdata

--- R/data.R
+++ R/data.R
@@ -1,16 +1,3 @@
-#' CONUS CDL codes for each maca location
-#'
-#' A dataset containing 2020 Cropland Data Layer Codes for all CONUS locations
-#'
-#' @format A data frame with 4218330 rows and 2 variables:
-#' \describe{
-#'   \item{location}{file name of maca grid cell with format "data_lat_long"}
-#'   \item{cdl_code_2020}{Crop Code from the 2020 cdl contained within each location}
-#' }
-#'
-#' @source \url{https://www.nass.usda.gov/Research_and_Science/Cropland/Release/index.php}
-"maca_2020_cdl"
-
 #'
 #'
 #' A list containing the names of 16 MACA models
